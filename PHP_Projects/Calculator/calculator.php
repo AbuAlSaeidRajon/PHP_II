2cf3822ba45762c9ae1874c1fb64bad21eb4551a
Saeid Md <khanrajon5@gmail.com>
2024-04-28 16:24:07 +0300

    calculator added

---
+++ PHP_Projects/Calculator/calculator.php
@@ -0,0 +1,31 @@
+<?php
+
+class Calculator {
+    private $num1;
+    private $num2;
+
+    public function __construct($num1, $num2) {
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+    }
+
+    public function add() {
+        return $this->num1 + $this->num2;
+    }
+
+    public function subtract() {
+        return $this->num1 - $this->num2;
+    }
+
+    public function multiply() {
+        return $this->num1 * $this->num2;
+    }
+
+    public function divide() {
+        if ($this->num2 == 0) {
+            return "Cannot divide by zero";
+        }
+        return $this->num1 / $this->num2;
+    }
+}
+?>
